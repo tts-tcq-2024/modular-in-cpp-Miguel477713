b9f3401f37eae5b0e9a853e563e925f7194cf054
Miguel477713 <131222467+Miguel477713@users.noreply.github.com>
2024-11-06 20:56:07 -0600

    Add files via upload
    Assignment: Separate to meet 50 lines per file

---
+++ ColorPair.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+#include <iostream>
+
+namespace TelCoColorCoder {
+    extern int numberOfMajorColors;
+    extern int numberOfMinorColors;
+    
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+}
+
+#endif
